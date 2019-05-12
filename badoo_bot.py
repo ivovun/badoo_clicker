fc78be799f2fc915f6559786f6c025d9f462dd12
Vladimir Filippov <sergeiych@gmail.com>
2019-05-12 03:54:20 +0300

    user name to log

--- badoo_bot.py
+++ badoo_bot.py
@@ -20,11 +20,12 @@ class WebDriver:
 def do_that_func_only_if_css_element_was_found(orig_func):
     def modified_func_with(**kwargs):
         try:
-            orig_func(**kwargs)
+            return orig_func(**kwargs)
         except NoSuchElementException:
             # print(f'Unable to locate element with css class:{kwargs.get("css_class_name", "")} ')
             pass
         except Exception as ex:
+            # time.sleep(60 * 60 * 24)  # strange error lets keep browser open
             return type(ex).__name__
     return modified_func_with
 
@@ -77,18 +78,18 @@ with WebDriver(webdriver.Chrome('./chromedriver')) as driver:
         else:
             was_clicked = 'dislike'
             click_btn_with(css_class='js-profile-header-vote-no', _driver=driver)
-        new_user_or_error_name = get_user_name()
+        new_user_or_error_name = get_user_name(_driver=driver)
         if new_user_or_error_name == old_user_or_error_name:
             unsuccessful_click_counter += 1
-            if unsuccessful_click_counter >= 2:
-                print(f"too many tries to click with no result, lets sleep to wait, user_or_error_name\
-                 = {new_user_or_error_name}")
+            if unsuccessful_click_counter >= 3:
+                print(f"too many tries to click with no result (maybe hit the vote limit), lets sleep to wait\
+                , user_or_error_name = {new_user_or_error_name}")
                 time.sleep(60 * 60 * 1)
                 unsuccessful_click_counter = 0
         else:
             unsuccessful_click_counter = 0
         old_user_or_error_name = new_user_or_error_name
-        print(f'{i}.  {was_clicked}   --- {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        print(f'{i}. {was_clicked}---{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}--user=>{old_user_or_error_name}')
         time.sleep(randint(1, 4))
 
 
