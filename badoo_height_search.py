f032cc996705e24c6d5eca4bc3e391ba9c308734
Vladimir Filippov <sergeiych@gmail.com>
2019-05-16 01:09:41 +0300

    added stop on girl with music

--- badoo_height_search.py
+++ badoo_height_search.py
@@ -7,6 +7,11 @@ from selenium.webdriver.support import expected_conditions
 import secret_keys
 import datetime
 import csv
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from pygame import mixer
 
 
 class WebDriver:
@@ -47,7 +52,9 @@ def do_that_func_only_if_css_element_was_found(orig_func):
             print(f' css class {kwargs.get("css_sel_or_xpath", "")} error = {err_name}')
             # time.sleep(60 * 60 * 24)  # strange error lets keep browser open
             return err_name
+
     return modified_func_with
+
 
 
 @do_that_func_only_if_css_element_was_found
@@ -60,23 +67,40 @@ def send_to_field_with(css_sel_or_xpath: str, keys: str, _driver: webdriver):
 
 @do_that_func_only_if_css_element_was_found
 def click_btn_with(css_sel_or_xpath: str, _driver: webdriver, need_to_login: bool = False, use_xpath: bool = False):
-
-    btn_login = _driver.find_element_by_xpath(css_sel_or_xpath) if use_xpath else\
+    btn_login = _driver.find_element_by_xpath(css_sel_or_xpath) if use_xpath else \
         _driver.find_element_by_css_selector(f'{css_sel_or_xpath}')
+    # btn_login = get_element_with_browser_delay(css_sel_or_xpath, _driver, By.XPATH if use_xpath else By.CSS_SELECTOR)
     btn_login.click()
-    time.sleep(random_float_number(0, 1))
+    time.sleep(random_float_number(1, 2))
     if need_to_login:
         login(_driver)
     return btn_login
 
+# @do_that_func_only_if_css_element_was_found
+# def find_btn_with(css_sel_or_xpath: str, _driver: webdriver, need_to_login: bool = False, use_xpath: bool = False):
+#     return _driver.find_element_by_xpath(css_sel_or_xpath) if use_xpath else \
+#         _driver.find_element_by_css_selector(f'{css_sel_or_xpath}')
+
+
+
+def get_element_with_browser_delay(css_sel_or_xpath: str, _driver: webdriver, element_located_by=By.CSS_SELECTOR):
+    delay = 3  # seconds
+    element = None
+    try:
+        # myElem = WebDriverWait(_driver, delay).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
+        element = WebDriverWait(_driver, delay).until(EC.presence_of_element_located((element_located_by
+                                                                                      , css_sel_or_xpath)))
+        # print("Page is ready!")
+
+    except TimeoutException:
+        print("Loading took too much time!")
+    finally:
+        return element
 
 
 @do_that_func_only_if_css_element_was_found
 def get_user_name(_driver: webdriver) -> str:
     return _driver.find_element_by_css_selector('.profile-header__name').text
-
-
-# class selenium.webdriver.support.expected_conditions.staleness_of(element)[source]
 
 
 def login(_driver: webdriver):
@@ -88,17 +112,20 @@ def login(_driver: webdriver):
 
 
 def random_float_number(a: int, b: int):
-    return randint(a * 10, b * 10)/10.0
+    return randint(a * 10, b * 10) / 10.0
 
 
-def return__element_by_xpath(xpath: str, _driver:webdriver):
-    try:
-        # class selenium.webdriver.support.expected_conditions.staleness_of(element)[source]
-        return _driver.find_element_by_xpath(xpath)
-    except NoSuchElementException:
-        pass
-        # print(f'Unable to locate element with css class:{kwargs.get("css_sel_or_xpath", "")} ')
-    return None
+@do_that_func_only_if_css_element_was_found
+def return__element_by_xpath(xpath: str, _driver: webdriver):
+    # try:
+    #     # return  get_element_with_browser_delay(css_sel_or_xpath, _driver
+    #     #                                                    , By.XPATH if use_xpath else By.CSS_SELECTOR)
+    return _driver.find_element_by_xpath(xpath)
+    #
+    # except NoSuchElementException:
+    #     pass
+    #     # print(f'Unable to locate element with css class:{kwargs.get("css_sel_or_xpath", "")} ')
+    # return None
 
 
 with WebDriver(webdriver.Chrome('./chromedriver')) as driver:
@@ -114,86 +141,57 @@ with WebDriver(webdriver.Chrome('./chromedriver')) as driver:
     while True:
         i += 1
         # deny blocking questions
+        time.sleep(random_float_number(0, 1))
         click_btn_with(css_sel_or_xpath='.js-chrome-pushes-deny', _driver=driver)
         click_btn_with(css_sel_or_xpath='.js-continue', _driver=driver)
         click_btn_with(css_sel_or_xpath='.icon.js-ovl-close', _driver=driver)  # new match close
         click_btn_with(css_sel_or_xpath='.js-session-expire', _driver=driver, need_to_login=True)
-        click_btn_with(css_sel_or_xpath='//div[@onclick="window.location.reload();', _driver=driver, need_to_login=True
+        click_btn_with(css_sel_or_xpath='//div[@onclick="window.location.reload();', _driver=driver
+                       , need_to_login=True
                        , use_xpath=True)
-
-        # //div[@onclick="window.location.reload();"]
-        time.sleep(random_float_number(1, 3))
-
+        # go to profile
         click_btn_with(css_sel_or_xpath='.b-link.js-profile-header-name.js-hp-view-element', _driver=driver)
         # river = webdriver.Chrome('./chromedriver')
-        time.sleep(random_float_number(3, 4))
-        # appearance_div_h = return__element_by_xpath("//b[contains(text(),'Appearance:')]", driver)
+        time.sleep(random_float_number(1, 3))
 
-        # https://xmltoolbox.appspot.com/xpath_generator.html
-        # https://www.softwaretestingclass.com/using-contains-sibling-ancestor-to-find-element-in-selenium/
-        # sibElements = driver.findElements(By.xpath("//a[contains(text()," +\
-        # "'Inside div block 2.')]/parent::div//following-sibling::div[@class='c']//a"));
-        appearance_div_h = return__element_by_xpath("//div[@class='form-label b']/b[contains(text(),\
-         'Appearance:')]/parent::div//following-sibling::div", driver)
-        # //div[@class='form-label b']/b[contains(text(), 'Appearance:')]/parent::div//following-sibling::div
-        # //div[@class='form-label b']/b[contains(text(), 'Appearance:')]/parent::div//following-sibling::div/text()
-        # "//div[@class='form-label b']/b[contains(text(), 'Appearance:')]/parent::div//following-sibling::div").text
+        # appearance_div_h = return__element_by_xpath("//div[@class='form-label b']/b[contains(text(),\
+        #  'Appearance:')]/parent::div//following-sibling::div", driver)
+        appearance_div_h = return__element_by_xpath(xpath="//div[@class='form-label b']/b[contains(text(),\
+         'Appearance:')]/parent::div//following-sibling::div", _driver=driver)
+
+        # appearance_div_h = get_element_with_browser_delay("//div[@class='form-label b']/b[contains(text(),\
+        #  'Appearance:')]/parent::div//following-sibling::div", driver, By.XPATH)
         if appearance_div_h is not None:
-            tup = tuple(range(150, 184))
+            tup = tuple(range(177, 184))
             girl_is_found = False
             print(f'testing --- {appearance_div_h.text}')
             for x in tup:
-                # ошибка тут - отрибут текст отсутвует иногда у appearance_div_h.text  нужно добавить проверку на наличие атрибута
                 if str(x) in appearance_div_h.text:
-                    # click_btn_with(css_sel_or_xpath='js-profile-header-vote-yes', _driver=driver)
-                    # File "/Users/vladimirfilippov/Dropbox/mbp/Developer/learning/py/dating/badoo/badoo_height_search.py", line 131, in <module>
-                    #     # //span[@class='profile-section__txt']
-                    # AttributeError: 'NoneType' object has no attribute 'text'
-
-                    about = return__element_by_xpath("//span[@class='profile-section__txt']", driver)
-                    # //span[@class='profile-section__txt']
-                    description_txt = f"!!!!!! ===>>>height = {x}, about={about.text if about is not None else '' }\
+                    about = return__element_by_xpath(xpath="//span[@class='profile-section__txt']", _driver=driver)
+                    description_txt = f"!!!==>>>height = {x}, about={about.text if about is not None else ''}\
                         appearance={appearance_div_h.text} "
                     driver.girls_set.add(description_txt)
                     print(description_txt)
-                    click_btn_with(css_sel_or_xpath='.js-profile-header-vote-yes', _driver=driver)
-                    girl_is_found = True
-                    break
+
+                    mixer.init()
+                    mixer.music.load("a2002011001-e02-128k.mp3")
+                    mixer.music.play()
+                    # time.sleep(100000)
+
+                    answer = input("continue y/n: ")
+                    if answer == 'y':
+                        click_btn_with(css_sel_or_xpath='.js-profile-header-vote-yes', _driver=driver)
+                        girl_is_found = True
+                        mixer.music.stop()
+                        break
+                    else:
+                        exit()
 
             if not girl_is_found:
                 click_btn_with(css_sel_or_xpath='.js-profile-header-vote-no', _driver=driver)
-
-            # time.sleep(4)
         else:
             click_btn_with(css_sel_or_xpath='.js-profile-header-vote-no', _driver=driver)
 
-        # time.sleep(10000)
         time.sleep(random_float_number(1, 2))
 
-
-        # was_clicked = 'like'
-        # if randint(0, 1) == 1:
-        #     click_btn_with(css_sel_or_xpath='js-profile-header-vote-yes', _driver=driver)
-        # else:
-        #     was_clicked = 'dislike'
-        #     click_btn_with(css_sel_or_xpath='js-profile-header-vote-no', _driver=driver)
-        # new_user_or_error_name = get_user_name(_driver=driver)
-        # if new_user_or_error_name == old_user_or_error_name:
-        #     unsuccessful_click_counter += 1
-        #     if unsuccessful_click_counter >= 3:
-        #         print(f"too many tries to click with no result (maybe hit the vote limit), lets sleep to wait\
-        #         , user_or_error_name = {new_user_or_error_name}")
-        #         time.sleep(60 * 60 * 1)
-        #         unsuccessful_click_counter = 0
-        # else:
-        #     unsuccessful_click_counter = 0
-        # old_user_or_error_name = new_user_or_error_name
-        # print(f'{i}. {was_clicked}---{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}--user=>{old_user_or_error_name}')
-
-
-
-
-
-
-# click_button(css_sel_or_xpath='js-chrome-pushes-deny', with_driver=driver)
-# time.sleep(randint(1, 3))
+# class selenium.webdriver.support.expected_conditions.staleness_of(element)[source]
